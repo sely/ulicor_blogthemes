70d0b6c7181040597cfddb1cba5e42bb3ee82153
sely <selyniax@gmail.com>
2014-08-31 23:08:31 +0200

    google analytics

--- ulicor/header.php
+++ ulicor/header.php
@@ -6,6 +6,16 @@
 <script src="<?php bloginfo('template_url'); ?>/js/snow.js" type="text/javascript"></script>
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+<script>
+	// google analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-54342496-1', 'auto');
+	ga('send', 'pageview');
+</script>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
